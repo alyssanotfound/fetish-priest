5923f63cd14d7eeb2063669f5f11fee9ac5fb393
alyssanotfound <davia317@gmail.com>
2020-04-09 18:07:54 -0400

    added ind button function

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,18 +1,13 @@
 var buttonson = false;
-var descon = false;
-
-console.log(buttonson);
-console.log(descon);
+var currItem;
 
 $(function() {
     $( ".shop" ).click(function() {
     	if (buttonson == true) {  		
     		$( ".circle" ).addClass('hidden');
     		buttonson = false;
-    		if (descon == true) {
-    			$( ".desc" ).addClass('hidden');
-    			descon = false;
-    		}
+    		//just in case any descriptions are on, loop thru and make sure they are all hidden
+    		$(".desc").addClass("hidden");
     	} else if (buttonson == false) {
     		$( ".circle" ).removeClass('hidden');
     		buttonson = true;
@@ -21,15 +16,12 @@ $(function() {
 });
 
 $(function() {
-    $( "#bertoia-wrapper .circle" ).click(function() {
-		if (buttonson == true && descon == false) {  
-			console.log("show descriptions");		
-    		$( ".desc" ).removeClass('hidden');
-    		descon = true;
-    	} else if (buttonson == true && descon == true) {
-    		console.log("hide descriptions");
-			$( ".desc" ).addClass('hidden');
-			descon = false;
-		}
+    $( ".circle" ).click(function() {
+    	currItem = $(this).siblings().hasClass("hidden");
+    	if (currItem == true) {
+    		$(this).siblings().removeClass("hidden");
+    	} else if (currItem == false) {
+    		$(this).siblings().addClass("hidden");
+    	}
 	});
 });
