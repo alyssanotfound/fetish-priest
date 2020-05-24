749e59697cdbdcc299c7114517efdae8347bc1d6
alyssanotfound <davia317@gmail.com>
2020-05-24 12:26:30 -0400

    update mobile button

--- public/js/functions.js
+++ public/js/functions.js
@@ -3,26 +3,26 @@ var currItem;
 var textOnDesktop = false;
 var textHover = false;
 var v;
+var ww;
 
 //CHECK BROWSER WIDTH, ADD/REMOVE CLASSES
 jQuery(document).ready(function($) {
-  var ww = document.body.clientWidth;
+  ww = document.body.clientWidth;
   //don't add hover shop effect if mobile bc is sticky
   if (ww >= 600) {
     $("<style type='text/css'> .shop p:hover{ background-color: yellow; color: blue;} </style>").appendTo("head");
   }
   var alterClass = function() {
+    ww = document.body.clientWidth;
     if (ww < 600) {
       v = "mobile";
       console.log("m resize, now: " + v);
       var vh = window.innerHeight * 0.01;
-      // Then we set the value in the --vh custom property to the root of the document
+      //set the value in the --vh custom property to the root of the document
       document.documentElement.style.setProperty('--vh', `${vh}px`);
       //find width of logo to place it evenly
       var logoW = $(".logo img").width();
-      // console.log(logoW);
       var dis = ((ww / 6) - logoW) / 2;
-      // console.log(dis);
       $(".logo").css({'left': dis  + 'px'});
       //remove inline height assigned in desktop mode, if there
       $(".textbox").css("height","");
@@ -30,45 +30,25 @@ jQuery(document).ready(function($) {
       $(".logo").css({'top':''});
       $(".textbox").addClass("hidden");
       $("#readmore").addClass("hidden");
-      // $(".textbox").addClass("hidden");
-      // var currH = calc(vh*80)
-      // $(".textbox").height(calc(vh * 80));
-      // $('.yellowbox span').addClass("remove");
-      // $("#references").removeClass("hidden");
       $(".toggletext").each(function(){
             revealText();
-            // $('.highlight').css("color", "yellow");
             textOnDesktop = true;
        });
       $(".dash").addClass("remove");
-      // alert("Size of window is, height: " + $(window).height() + ", width: " + $(window).width());
     } else if (ww >= 601) {
       v = "desktop";
       console.log("d resize, now: " + v);
       $(".textbox").removeClass("hidden");
       $(".textbox").height(($(window).height() - $(".shop").outerHeight()) +"px");
-      $(".logo").css("left","");
-      // var position = $(".creditstext").position();
+      $(".logo").css('left','');
       var position = $('.info-desktop').offset();
       var h = $('.info-desktop').outerHeight();
-      // console.log(h);
-      // console.log(position);
       var mTop = position.top + h;
-      // console.log(mTop);
       $("#mc_embed_signup").css({'top': mTop  + 'px'});
-      // $(".logo").css({'top': mTop  + 'vh'});
-      // console.log("window height is: " + $(window).height());
-      // console.log("shop height is: " + $(".shop").outerHeight());
-      // console.log("default yellowbox height: " + $(".yellowbox").outerHeight());
-      // console.log("yellowbox height should be: " + ((($(window).height())/3) - $(".shop").outerHeight()) +"px");
       $(".yellowbox").outerHeight(((($(window).height())/3) - $(".shop").outerHeight()) +"px");
-      // console.log("yellowbox actual height: " + $(".yellowbox").outerHeight());
       $("#readmore").removeClass("hidden");
-      // $('.yellowbox span').removeClass("remove");
-      // $("#references").addClass("hidden");
       $(".toggletext").each(function() {
             hideText();
-            // $('.highlight').css("color", "white");
             textOnDesktop = false;
        });
       $(".dash").removeClass("remove");
@@ -85,7 +65,6 @@ jQuery(document).ready(function($) {
 //SHOP BUTTON
 $(function() {
     $( ".shop" ).click(function() {
-      // e.preventDefault();
       var ww = document.body.clientWidth;
     	if (buttonson == true) {  		
     		$( ".circle" ).addClass('hidden');
@@ -93,18 +72,11 @@ $(function() {
     		$(".desc").addClass("hidden");
         //remove item divs so they arent overlapping other info
         $(".wrap").addClass('remove');
-        // $(".shop p").removeClass('yellowOn');
         if (v == "mobile") {
           $(".shop").removeClass('yellowOn'); 
         } else if (v == "desktop") {
           $(".shop p").removeClass('yellowOn');
         }
-        // $(".shop p").css({
-        //     color: "white", 
-        //     backgroundColor: "blue"
-        // });
-        // alert("highlight off");
-        // alert($(".shop p").attr("class"));
         buttonson = false;
     	} else if (buttonson == false) {
         //make divs visible
@@ -116,33 +88,15 @@ $(function() {
             $('.info-button').css("background-image", "url(../assets/info-button.png");
         }
 		    $( ".circle" ).removeClass('hidden');
-        
-        // $(".shop p").addClass('yellowOn');
         if (v == "mobile") {
           $(".shop").addClass('yellowOn'); 
         } else if (v == "desktop") {
           $(".shop p").addClass('yellowOn');
         }
-        // alert("highlight on");
-        // alert($(".shop p").attr("class"));
-        // $(".shop p").css({
-        //     color: "blue", 
-        //     backgroundColor: "yellow"
-        // });
         buttonson = true;
       }
 	});
 });
-
-//fix shop button hover issue
-// function fix() {
-//   var el = this;
-//   var par = el.parentNode;
-//   var next = el.nextSibling;
-//   alert(el);
-//   par.removeChild(el);
-//   setTimeout(function() {par.insertBefore(el, next);}, 0)
-// }
 
 //INDIVIDUAL PRODUCT BUTTONS
 $(function() {
@@ -162,27 +116,22 @@ $(function() {
         if ($(".textbox").hasClass("hidden")==true) {
             //this is if the text box isn't visible yet, so check if shop buttons are on
             if (buttonson == true) { 
-            console.log("trying to open textbox, buttons are on, turn them off.");       
-            $( ".circle" ).addClass('hidden');
-            buttonson = false;
-            //just in case any descriptions are on, loop thru and make sure they are all hidden
-            $(".desc").addClass("hidden");
-            //remove item divs so they arent overlapping other info
-            $(".wrap").addClass('remove');
-            // $(".shop p").removeClass('yellowOn');
-            if (v == "mobile") {
-              $(".shop").removeClass('yellowOn'); 
-            } else if (v == "desktop") {
-              $(".shop p").removeClass('yellowOn');
-            }
-            // $(".shop p").css({
-            //     color: "white", 
-            //     backgroundColor: "blue"
-            // });
+              console.log("trying to open textbox, buttons are on, turn them off.");       
+              $( ".circle" ).addClass('hidden');
+              buttonson = false;
+              //just in case any descriptions are on, loop thru and make sure they are all hidden
+              $(".desc").addClass("hidden");
+              //remove item divs so they arent overlapping other info
+              $(".wrap").addClass('remove');
+              // $(".shop p").removeClass('yellowOn');
+              if (v == "mobile") {
+                $(".shop").removeClass('yellowOn'); 
+              } else if (v == "desktop") {
+                $(".shop p").removeClass('yellowOn');
+              }
             } 
             //now open textbox
             $(".textbox").removeClass("hidden");
-            // $(".textbox").height(($(window).height() - $(".shop").outerHeight()) +"px");
             $('.info-button').css("background-image", "url(../assets/info-button-x.png");
         } else if ($(".textbox").hasClass("hidden")==false) {
             //textbox is already open, so close it
@@ -222,10 +171,6 @@ $(function() {
         textHover = true;
       }, function() {
         //will never trigger anything when mouseOff 'read more' because it'll always be a hover state of div
-        // console.log("hide");
-        // if (textOnDesktop == false){
-        //     hideText();
-        // }
       }
     );
 });
@@ -233,13 +178,10 @@ $(function() {
 $(function() {
     $( ".textbox" ).hover(
       function() {
-        // console.log("mouse in on textbox");
         if (textHover == true ){
            revealText(); 
         }
       }, function() {
-        // console.log("mouse out in on textbox");
-        // console.log("hide");
         if (textOnDesktop == false){
         hideText();
         textHover = false;
@@ -247,8 +189,6 @@ $(function() {
       }
     );
 });
-
-// $('#readmore').is(":hover") == false && 
 
 function revealText() {
     $(".toggletext").removeClass( "hidden" );
@@ -276,14 +216,6 @@ $(function() {
 });
 
 //CREDITS - DESKTOP
-
-// window.jsConveyor1 = "hello";
-// $(function() {
-//     window.jsConveyor1 = $('.js-conveyor-1').jConveyorTicker({
-//       start_paused: true
-//     });
-// });
-
 $(function() {
     $(".credits").click(function(){
         // $(".creditstext").toggle();
@@ -309,8 +241,7 @@ $(function() {
     $("#mc_embed_signup").toggle();
     $(".credits").toggle();
     if ($('.creditstext').css('visibility') == 'visible') {
-        $('.creditstext').css('visibility','hidden');
-        // jsConveyor1.pauseAnim();
+      $('.creditstext').css('visibility','hidden');
     }
   });
 });
